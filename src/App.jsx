ad2591a9fcd4cac00d5bd18b4528e104b7176c32
samarthchouhan07 <samarthchouhan29544@gmail.com>
2025-07-20 18:20:47 +0530

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ const Layout = () => (
   <ErrorBoundary>
     <div>
       <Header />
-      <div className="pt-16">
+      <div className="pt-16 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <Outlet />
       </div>
     </div>
